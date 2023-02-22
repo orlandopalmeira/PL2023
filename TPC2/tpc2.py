7a7816a5fb37c479390487b35b347fac76fc9265
orlandopalmeira <orlandopalmeira51@gmail.com>
2023-02-22 14:16:30 +0000

    TPC2 melhorado

--- TPC2/tpc2.py
+++ TPC2/tpc2.py
@@ -1,39 +1,27 @@
-import sys
 import re
-from utils import multi_split, dig_seqs
+import sys
 
-# Esta regex serve para encontrar todas as ocorrências da palavra "On" (em qualquer combinação
-# de maiúsculas e minúsculas) no texto
-regex_on = r'[oO][nN]'
+# Esta função retorna todos as sequências de dígitos de uma dada string na forma de números inteiros.
+def dig_seqs(string: str):
+    regex = r'([0-9]+)+' # expressão regular para números inteiros
+    seqs = re.findall(regex, string) # procura todas as sequências de dígitos
+    return [int(x) for x in seqs] # transforma as sequências em números inteiros
 
-# Esta regex serve para encontrar todas as ocorrências da palavra "Off" (em qualquer combinação
-# de maiúsculas e minúsculas) no texto
-regex_off = r'[oO][fF][fF]'
-
-# Esta regex serve para encontrar todas as sequências de dígitos num texto
-regex_int = r'([0-9]+)+'
-
-# Esta regex serve para encontrar o símbolo '=' no texto
-regex_eq = r'='
-
-# Lê do STDIN até ao EOF (CTRL + D)
 text = sys.stdin.read()
 
-# Indica se o comportamento de efectuar a soma está activado
-on = True
+regex = r'\d+|[oO][nN]|[oO][fF][fF]|='
 
-# Contém a soma total
+partes = re.findall(regex, text)
+
+on = True
 soma = 0
 
-# Contém a string partida convenientemente segundo os padrões mencionados no enunciado.
-partes = multi_split([regex_on, regex_off, regex_eq], text)
-
 for parte in partes:
-    if re.fullmatch(regex_on, parte): # parte é a palavra "On"
+    if re.fullmatch(r'[oO][nN]', parte): # parte é a palavra "On"
         on = True
-    elif re.fullmatch(regex_off, parte): # parte é a palavra "Off"
+    elif re.fullmatch(r'[oO][fF][fF]', parte): # parte é a palavra "Off"
         on = False
-    elif re.fullmatch(regex_eq, parte): # parte é o símbolo "="
+    elif re.fullmatch(r'=', parte): # parte é o símbolo "="
         print(soma)
     else: 
         soma += sum(dig_seqs(parte)) if on else 0
